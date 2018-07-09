d0c440c5bda6a1074b10f4c6bd0bc604a330ac2d
Andrew Garner <andrewgarner101@gmail.com>
2018-07-08 22:02:12 -0700

    LCD Hello World

---
+++ LcdHelloWorld/LcdHelloWorld.ino
@@ -0,0 +1,36 @@
+/**
+   Simple Hello World sketch for LCD
+
+   Using:
+   SparkFun Serial LCD
+   Arduino UNO R3
+
+   Andrew Garner
+   July 8, 2018
+*/
+
+#include <SoftwareSerial.h>
+
+// Attach the serial enabled LCD's RX line to digital pin 11
+SoftwareSerial LCD(10, 11); // Arduino SS_RX = pin 10 (unused), Arduino SS_TX = pin 11
+
+void setup() {
+  LCD.begin(9600); // set up serial port for 9600 baud
+  delay(500); // wait for display to boot up
+}
+
+void loop() {
+  //Clear display
+  LCD.write(0xFE);
+  LCD.write(0x01);
+
+  //Move cursor to the beginning of the first line
+  LCD.write(0xFE);
+  LCD.write(0x80);
+
+  LCD.write("Hello, World!");
+
+  //Prevent loop from continuing.
+  while (1);
+
+}
